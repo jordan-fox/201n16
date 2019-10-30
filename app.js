f5156ccf0b4b6780cd4ec5a34cfa2610ee41d013
Jordan Fox <jordanmichaelfox@hotmail.com>
2019-10-29 20:46:23 -0700

    Made working alerts instead of console logs

--- app.js
+++ app.js
@@ -1,58 +1,68 @@
 'use strict';
 
 var userName = prompt('Hello, what is your name?');
-console.log('User Name: ' + userName);
+// console.log('User Name: ' + userName);
 
 alert(userName + ' welcome to my page!');
 
 var whereBorn = prompt('Was I born in Seattle?');
 whereBorn = whereBorn.toLowerCase();
-console.log(userName + ' answered ' + whereBorn + ' to if I was born in Seattle.');
+// console.log(userName + ' answered ' + whereBorn + ' to if I was born in Seattle.');
 
 if (whereBorn.toLowerCase() === 'yes' || whereBorn.toLowerCase() === 'y') {
-    console.log('Nailed it!');
+    // console.log('Nailed it!');
+    alert(userName + ', you nailed it!');
 } else {
-    console.log('You are wrong!')
-}
+    // console.log('You are wrong!');
+    alert(userName + ', you\'re so wrong!');
+};
 
 var whereBefore = prompt('Did I live in England before here?');
 whereBefore = whereBefore.toLowerCase();
-console.log(userName + ' answered ' + whereBefore + ' to if I lived in England before here.');
+// console.log(userName + ' answered ' + whereBefore + ' to if I lived in England before here.');
 
 if (whereBefore.toLowerCase() === 'no' || whereBefore.toLowerCase() === 'n') {
-    console.log('Perfect!');
+    // console.log('Perfect!');
+    alert(userName + ', what a superb answer!');
 } else {
-    console.log('So not right!')
-}
+    // console.log('So not right!');
+    alert(userName + ', not even close!');
+};
 
 var olderBrother = prompt('Do I have an older brother?');
 olderBrother = olderBrother.toLowerCase();
 console.log(userName + ' answered ' + olderBrother + ' to if I have an older brother.');
 
 if (olderBrother.toLowerCase() === 'yes' || olderBrother.toLowerCase() === 'y') {
-    console.log('Well done!');
+    // console.log('Well done!');
+    alert(userName + ', well done!');
 } else {
-    console.log('Nope!')
-}
+    // console.log('Nope!');
+    alert(userName + ', I disagree!');
+};
 
 var oldSchool = prompt('Did I go to Auckland University of Technology?');
 oldSchool = oldSchool.toLowerCase();
-console.log(userName + ' answered ' + oldSchool + ' to if I attended Auckland University of Technology.');
+// console.log(userName + ' answered ' + oldSchool + ' to if I attended Auckland University of Technology.');
 
 if (oldSchool.toLowerCase() === 'yes' || oldSchool.toLowerCase() === 'y') {
-    console.log('Excellent!');
+    // console.log('Excellent!');
+    alert(userName + ', excellent work!');
 } else {
-    console.log('Definitely not right!')
-}
+    // console.log('Definitely not right!');
+    alert(userName + ', definitely not right!');
+};
 
 var oldJob = prompt('Did I work as a paramedic?');
 oldJob = oldJob.toLowerCase();
-console.log(userName + ' answered ' + oldJob + ' to if I worked as a paramedic.');
+// console.log(userName + ' answered ' + oldJob + ' to if I worked as a paramedic.');
 
 if (oldJob.toLowerCase() === 'yes' || oldJob.toLowerCase() === 'y') {
-    console.log('Perfect!');
+    // console.log('Perfect!');
+    alert(userName + ', truly perfect!');
 } else {
-    console.log('Nope!')
-}
+    // console.log('Nope!');
+    alert(userName + ', so wrong!');
+};
 
-alert(userName + ' thank you for playing!')
+alert(userName + ' thank you for playing!');
